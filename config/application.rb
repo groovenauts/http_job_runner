832dc942a4de1eabcf005556509b722ccaa18702
akm <akm2000@gmail.com>
2016-04-18 19:52:26 +0900

    Set the queue_adapter

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module HttpJobRunner
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # https://github.com/collectiveidea/delayed_job#rails-42
+    config.active_job.queue_adapter = :delayed_job
   end
 end
