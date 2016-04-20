d7518e5fdefa7f9671fba4e5dacf5db5a603d2ab
akm <akm2000@gmail.com>
2016-04-20 11:17:32 +0900

    Update delayed_job factory attributes

--- spec/factories/delayed_jobs.rb
+++ spec/factories/delayed_jobs.rb
@@ -1,14 +1,20 @@
 FactoryGirl.define do
   factory :delayed_job, class: 'Delayed::Job' do
-    priority 1
-    attempts 1
-    handler "MyText"
-    last_error "MyText"
+    priority 0
+    attempts 0
+    handler [
+      '--- !ruby/object:Delayed::PerformableMethod',
+      "object: !ruby/module 'LoggerPipe'",
+      'method_name: :run',
+      'args:',
+      '- date',
+    ].join("\n")
+    last_error nil
     run_at "2016-04-20 10:45:20"
-    locked_at "2016-04-20 10:45:20"
-    failed_at "2016-04-20 10:45:20"
-    locked_by "MyString"
-    queue "MyString"
+    locked_at nil
+    failed_at nil
+    locked_by nil
+    queue nil
     created_at "2016-04-20 10:45:20"
     updated_at "2016-04-20 10:45:20"
   end
