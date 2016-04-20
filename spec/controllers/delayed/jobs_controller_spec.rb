1fb0ceed78f33696b1777627888bef1232a90c85
akm <akm2000@gmail.com>
2016-04-20 11:48:08 +0900

    define valid_attributes of controller spec

--- spec/controllers/delayed/jobs_controller_spec.rb
+++ spec/controllers/delayed/jobs_controller_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe Delayed::JobsController, type: :controller do
   # Delayed::Job. As you add validations to Delayed::Job, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    FactoryGirl.attributes_for(:delayed_job)
   }
 
   let(:invalid_attributes) {
