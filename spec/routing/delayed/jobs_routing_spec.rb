6c317bdd675c8b6d5b431bd47f43d37958aa0d32
akm <akm2000@gmail.com>
2016-04-20 12:37:11 +0900

    Remove "/delayed" from request path

--- spec/routing/delayed/jobs_routing_spec.rb
+++ spec/routing/delayed/jobs_routing_spec.rb
@@ -4,35 +4,35 @@ RSpec.describe Delayed::JobsController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/delayed/jobs").to route_to("delayed/jobs#index")
+      expect(:get => "/jobs").to route_to("delayed/jobs#index")
     end
 
     it "routes to #new" do
-      expect(:get => "/delayed/jobs/new").to route_to("delayed/jobs#new")
+      expect(:get => "/jobs/new").to route_to("delayed/jobs#new")
     end
 
     it "routes to #show" do
-      expect(:get => "/delayed/jobs/1").to route_to("delayed/jobs#show", :id => "1")
+      expect(:get => "/jobs/1").to route_to("delayed/jobs#show", :id => "1")
     end
 
     it "routes to #edit" do
-      expect(:get => "/delayed/jobs/1/edit").to route_to("delayed/jobs#edit", :id => "1")
+      expect(:get => "/jobs/1/edit").to route_to("delayed/jobs#edit", :id => "1")
     end
 
     it "routes to #create" do
-      expect(:post => "/delayed/jobs").to route_to("delayed/jobs#create")
+      expect(:post => "/jobs").to route_to("delayed/jobs#create")
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/delayed/jobs/1").to route_to("delayed/jobs#update", :id => "1")
+      expect(:put => "/jobs/1").to route_to("delayed/jobs#update", :id => "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/delayed/jobs/1").to route_to("delayed/jobs#update", :id => "1")
+      expect(:patch => "/jobs/1").to route_to("delayed/jobs#update", :id => "1")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/delayed/jobs/1").to route_to("delayed/jobs#destroy", :id => "1")
+      expect(:delete => "/jobs/1").to route_to("delayed/jobs#destroy", :id => "1")
     end
 
   end
