c3cb2a2867b0a3269d1063331fc998875a9c8a7e
akm <akm2000@gmail.com>
2016-04-21 15:23:52 +0900

    Add the simplest example application.

---
+++ examples/hello_world/hello_world.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+LANG=C
+d=$(date) && echo "${d}: Hello,"; sleep 2
+d=$(date) && echo "${d}: ."; sleep 2
+d=$(date) && echo "${d}: ."; sleep 2
+d=$(date) && echo "${d}: ."; sleep 2
+d=$(date) && echo "${d}: world!"
